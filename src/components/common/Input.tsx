aef6ffca7088b9dc760fdb3dc196512d7f75b1fd
max254 <maxkamau77@gmail.com>
2026-01-17 18:21:36 +0300

    Reusable Input data ui

---
+++ src/components/common/Input.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { cn } from "../../utils/cn";
+
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  label?: string;
+}
+
+export const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className, label, ...props }, ref) => {
+    return (
+      <div className="flex flex-col gap-1">
+        {label && (
+          <label className="text-sm font-medium text-gray-700">{label}</label>
+        )}
+        <input
+          ref={ref}
+          className={cn(
+            "flex h-10 w-full rounded-md border border-gray-300 bg-white px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent",
+            className
+          )}
+          {...props}
+        />
+      </div>
+    );
+  }
+);
