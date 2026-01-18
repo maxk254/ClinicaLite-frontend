dc0e30724415f586f46e92cad057a8e407b4d5a9
max254 <maxkamau77@gmail.com>
2026-01-18 15:59:06 +0300

    updated the dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -4,7 +4,7 @@ import { toast } from "react-hot-toast";
 import { Layout } from "../components/layout/Layout";
 import { PatientForm } from "../components/features/PatientForm";
 import { PatientList } from "../components/features/PatientList";
-import { PatienAPI } from "../api/patientApi";
+import { patientAPI } from "../api/patientApi";
 import type { Patient } from "../types/patient";
 
 export const Dashboard: React.FC = () => {
@@ -14,7 +14,7 @@ export const Dashboard: React.FC = () => {
   // Function to fetch data from your custom DB
   const fetchPatients = async () => {
     try {
-      const data = await PatienAPI.getAll();
+      const data = await patientAPI.getAll();
       setPatients(data);
     } catch (error) {
       toast.error("Connection Error: Could not fetch patients.");
@@ -31,11 +31,31 @@ export const Dashboard: React.FC = () => {
   const handleDelete = async (id: string) => {
     if (!confirm("Are you sure you want to discharge this patient?")) return;
     try {
-      await PatienAPI.delete(id);
+      await patientAPI.delete(id);
       toast.success("Patient discharged.");
       fetchPatients(); // Refresh list
     } catch (error) {
       toast.error("Failed to delete.");
+    }
+  };
+
+  const handleEdit = async (patient: Patient) => {
+    // 1. Ask the user for the new status
+    const newStatus = prompt(
+      "Update Status (Admitted / Discharged / ICU):",
+      patient.status
+    );
+
+    
+    if (newStatus && newStatus !== patient.status) {
+      try {
+        // 3. Send it to the backend
+        await patientAPI.update({ ...patient, status: newStatus });
+        toast.success("Status updated!");
+        fetchPatients(); // Refresh the table
+      } catch (error) {
+        toast.error("Update failed.");
+      }
     }
   };
 
@@ -56,6 +76,7 @@ export const Dashboard: React.FC = () => {
             patients={patients}
             isLoading={loading}
             onDelete={handleDelete}
+            onEdit={handleEdit}
           />
         </div>
       </div>
